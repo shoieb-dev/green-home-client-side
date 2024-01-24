f6f586f8300331a4e08558ac8227548df1eb872c
Shoieb Alam <shoieb.ctg@gmail.com>
2024-01-25 00:04:54 +0600

    add API file

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+// api.js
+const API_BASE_URL = "https://green-home-server-side.vercel.app";
+
+export const API_ENDPOINTS = {
+    houses: `${API_BASE_URL}/houses`,
+    reviews: `${API_BASE_URL}/reviews`,
+    users: `${API_BASE_URL}/users`,
+    bookings: `${API_BASE_URL}/bookings`,
+    myApartments: `${API_BASE_URL}/myApartments`,
+};
