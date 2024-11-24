ee3ae9df449c15008a6965aa7d12fb72cc3ea194
Shoieb Alam <shoieb-alam@users.noreply.github.com>
2024-11-24 22:58:31 +0600

    refactored & add loader

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,49 +1,72 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Carousel, Spinner } from 'react-bootstrap';
 import './Banner.css';
 
+const slides = [
+    {
+        src: "https://i.ibb.co/rmB3JSQ/image.png",
+        alt: "First slide",
+        caption: "IT'S GOOD TO <br /> TOUCH THE GREEN",
+    },
+    {
+        src: "https://i.ibb.co/Pr252nz/image.png",
+        alt: "Second slide",
+        caption: "CHOOSE YOUR HOME <br /> IN NATURE",
+    },
+    {
+        src: "https://i.ibb.co/xqL8X1M/image.png",
+        alt: "Third slide",
+        caption: "BE GREEN <br /> BE HAPPY",
+    },
+];
+
 const Banner = () => {
+    const [loadedImages, setLoadedImages] = useState(Array(slides.length).fill(false));
+
+    const handleImageLoad = (index) => {
+        const updatedLoadedImages = [...loadedImages];
+        updatedLoadedImages[index] = true;
+        setLoadedImages(updatedLoadedImages);
+    };
+
     return (
         <div id="banner">
-            <>
-                <Carousel fade>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="https://i.ibb.co/rmB3JSQ/image.png"
-                            alt="First slide"
-                        />
+            <Carousel fade>
+                {slides.map((slide, index) => (
+                    <Carousel.Item key={index}>
+                        <div style={{ position: "relative", height: "600px", width: "100%" }}>
+                            {/* Loader */}
+                            {!loadedImages[index] && (
+                                <div className='spinner-container'>
+                                    <Spinner
+                                        animation="border"
+                                        variant="success"
+                                        style={{ height: "100px", width: "100px" }}
+                                    />
+                                </div>
+                            )}
 
+                            {/* Image */}
+                            <img
+                                className="d-block w-100"
+                                src={slide.src}
+                                alt={slide.alt}
+                                style={{ height: "600px", display: loadedImages[index] ? "block" : "none", }}
+                                onLoad={() => handleImageLoad(index)}
+                            />
+                        </div>
+
+                        {/* Caption */}
                         <Carousel.Caption>
-                            <h1 className="banner-text">IT'S GOOD TO <br /> TOUCH THE GREEN</h1>
+                            <h1
+                                className="banner-text"
+                                style={{ display: loadedImages[index] ? "block" : "none" }}
+                                dangerouslySetInnerHTML={{ __html: slide.caption }}
+                            />
                         </Carousel.Caption>
                     </Carousel.Item>
-
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="https://i.ibb.co/Pr252nz/image.png"
-                            alt="Second slide"
-                        />
-
-                        <Carousel.Caption>
-                            <h1 className="banner-text">CHOOSE YOUR HOME <br /> IN NATURE </h1>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="https://i.ibb.co/xqL8X1M/image.png"
-                            alt="Third slide"
-                        />
-
-                        <Carousel.Caption>
-                            <h1 className="banner-text">BE GREEN <br /> BE HAPPY</h1>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-            </>
+                ))}
+            </Carousel>
         </div>
     );
 };
