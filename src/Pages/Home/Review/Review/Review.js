a06c041a8559719931a937d0697ac969d9dd53d4
shoieb-alam <shoieb.ctg@gmail.com>
2025-07-02 12:29:18 +0600

    refactor Review component for improved styling and code consistency

--- src/Pages/Home/Review/Review/Review.js
+++ src/Pages/Home/Review/Review/Review.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import { faQuoteLeft, faStar } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { Card } from "react-bootstrap";
+import { faQuoteLeft, faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Review = ({ review }) => {
     const { name, reviewtext, img } = review;
 
     return (
-        // Review Cards 
+        // Review Cards
         <div className="p-3">
-
             <Card className="card h-100 bg-light">
-                <Card.Img variant="top" src={img} className="w-50 mx-auto" />
+                <Card.Img variant="top" src={img} className="w-25 rounded-circle mx-auto" />
 
                 <Card.Body>
                     <FontAwesomeIcon icon={faQuoteLeft} className="" />
