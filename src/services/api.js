344b980642a1fb8eaf30eb6e90bdf0ad4d36f6c0
shoieb-alam <shoieb.ctg@gmail.com>
2025-08-05 19:35:42 +0600

    feat: export API_BASE_URL for use in other modules

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // api.js
 const serverUrl = "https://green-home-server-side.vercel.app";
 const localUrl = "http://localhost:5000";
-const API_BASE_URL = serverUrl;
+export const API_BASE_URL = serverUrl;
 
 export const API_ENDPOINTS = {
     houses: `${API_BASE_URL}/houses`,
