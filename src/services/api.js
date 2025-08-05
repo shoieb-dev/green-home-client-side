b3b817c09b1f63a7a6227336f8bb89dc759e616d
shoieb-alam <shoieb.ctg@gmail.com>
2025-08-05 20:55:23 +0600

    fix: update server and local URLs to include API path

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 // api.js
-const serverUrl = "https://green-home-server-side.vercel.app";
-const localUrl = "http://localhost:5000";
+const serverUrl = "https://green-home-server-side.vercel.app/api";
+const localUrl = "http://localhost:5000/api";
 export const API_BASE_URL = serverUrl;
 
 export const API_ENDPOINTS = {
