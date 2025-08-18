5f4a6e0110066f687c3f38d59d2a4e179bfc5da0
shoieb-alam <shoieb.ctg@gmail.com>
2025-08-18 12:47:17 +0600

    refactor(upgrade App.jsx with PublicLayout and DashboardLayout):

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PublicLayout from "./components/layout/PublicLayout/PublicLayout";
+import DashboardLayout from "./components/layout/DashboardLayout/DashboardLayout";
 import "./App.css";
-import Footer from "./components/layout/Footer/Footer";
-import Header from "./components/layout/Header/Header";
+import PrivateRoute from "./Pages/auth/PrivateRoute/PrivateRoute";
 import AuthProvider from "./contexts/AuthProvider";
+
+// Public pages
+import Home from "./Pages/Home/Home/Home";
 import Apartments from "./Pages/Apartments/Apartments/Apartments";
 import Login from "./Pages/auth/Login/Login";
-import PrivateRoute from "./Pages/auth/PrivateRoute/PrivateRoute";
 import Register from "./Pages/auth/Register/Register";
 import Booking from "./Pages/Booking/Booking/Booking";
+import NotFound from "./Pages/NotFound/NotFound";
+
+// Dashboard pages
+import Dashboard from "./Pages/Dashboard/Dashboard/Dashboard";
 import BookingForm from "./Pages/Booking/Booking/BookingForm";
 import ApartmentAdding from "./Pages/Dashboard/ApartmentAdding/ApartmentAdding";
 import ApartmentManaging from "./Pages/Dashboard/ApartmentManaging/ApartmentManaging";
 import MakeAdmin from "./Pages/Dashboard/MakeAdmin/MakeAdmin";
 import ManageAllBookings from "./Pages/Dashboard/ManageAllBookings/ManageAllBookings";
-import Home from "./Pages/Home/Home/Home";
 import ReviewAdding from "./Pages/Home/Review/ReviewAdding/ReviewAdding";
 import MyApartments from "./Pages/MyApartments/MyApartments";
-import NotFound from "./Pages/NotFound/NotFound";
 import Payment from "./Pages/Payment/Payment";
 
 function App() {
@@ -26,81 +31,37 @@ function App() {
         <div className="App">
             <AuthProvider>
                 <Router>
-                    <Header />
                     <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/home" element={<Home />} />
-                        <Route path="/apartments" element={<Apartments />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/booking/:houseId" element={<Booking />} />
+                        {/* Public layout */}
+                        <Route element={<PublicLayout />}>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/home" element={<Home />} />
+                            <Route path="/apartments" element={<Apartments />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/register" element={<Register />} />
+                            <Route path="/booking/:houseId" element={<Booking />} />
+                            <Route path="*" element={<NotFound />} />
+                        </Route>
+
+                        {/* Dashboard layout (protected) */}
                         <Route
-                            path="/booking/:houseId/bookingForm"
                             element={
                                 <PrivateRoute>
-                                    <BookingForm />
+                                    <DashboardLayout />
                                 </PrivateRoute>
                             }
-                        />
-                        <Route
-                            path="/bookings"
-                            element={
-                                <PrivateRoute>
-                                    <MyApartments />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/reviewAdding"
-                            element={
-                                <PrivateRoute>
-                                    <ReviewAdding />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/payment"
-                            element={
-                                <PrivateRoute>
-                                    <Payment />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/makeAdmin"
-                            element={
-                                <PrivateRoute>
-                                    <MakeAdmin />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/addApartment"
-                            element={
-                                <PrivateRoute>
-                                    <ApartmentAdding />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/manageApartments"
-                            element={
-                                <PrivateRoute>
-                                    <ApartmentManaging />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/manageAllBookings"
-                            element={
-                                <PrivateRoute>
-                                    <ManageAllBookings />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route path="*" element={<NotFound />} />
+                        >
+                            <Route path="/dashboard" element={<Dashboard />} />
+                            <Route path="/booking/:houseId/bookingForm" element={<BookingForm />} />
+                            <Route path="/bookings" element={<MyApartments />} />
+                            <Route path="/reviewAdding" element={<ReviewAdding />} />
+                            <Route path="/payment" element={<Payment />} />
+                            <Route path="/manageAllBookings" element={<ManageAllBookings />} />
+                            <Route path="/manageApartments" element={<ApartmentManaging />} />
+                            <Route path="/addApartment" element={<ApartmentAdding />} />
+                            <Route path="/makeAdmin" element={<MakeAdmin />} />
+                        </Route>
                     </Routes>
-                    <Footer />
                 </Router>
             </AuthProvider>
         </div>
