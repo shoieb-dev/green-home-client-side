9c293fe7be8320363e2b785a1c65e5f675a2db2b
shoieb-alam <shoieb.ctg@gmail.com>
2025-08-22 11:49:27 +0600

    add vitejs/plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [tailwindcss(), tsconfigPaths()],
+    plugins: [tailwindcss(), tsconfigPaths(), react({ jsxRuntime: "automatic" })],
 });
