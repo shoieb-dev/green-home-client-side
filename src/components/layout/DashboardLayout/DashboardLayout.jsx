473f32db99dbc14cd04972e1e630912d83dad1f6
shoieb-alam <shoieb.ctg@gmail.com>
2025-08-28 11:29:52 +0600

    update dashboard layout margin on sidebar collapse

--- src/components/layout/DashboardLayout/DashboardLayout.jsx
+++ src/components/layout/DashboardLayout/DashboardLayout.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar";
 import Header from "../Header/Header";
+import { useSidebar } from "../../../contexts/SidebarContext";
 
 export default function DashboardLayout() {
+    const { isCollapsed } = useSidebar();
+
     return (
         <>
             <Header />
             <div className="flex min-h-screen">
                 <Sidebar />
-                <main className="flex-1 bg-gray-100 ml-72">
+                <main className={`flex-1 bg-gray-100 mt-16 ${isCollapsed ? "ml-20" : "ml-72"}`}>
                     <Outlet />
                 </main>
             </div>
