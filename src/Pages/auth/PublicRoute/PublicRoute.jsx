4c91d3ffd109c12af79ef6471e2ae4877863e54a
shoieb-alam <shoieb.ctg@gmail.com>
2025-09-16 17:53:00 +0600

    add private route component

---
+++ src/Pages/auth/PublicRoute/PublicRoute.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import Loader from "../../../components/Loader/Loader";
+import useAuth from "../../../hooks/useAuth";
+
+const PublicRoute = ({ children }) => {
+    const { user, isCheckingUser } = useAuth();
+
+    if (isCheckingUser) return <Loader />;
+
+    // If logged in, redirect to dashboard (or home)
+    return user?.email ? <Navigate to="/dashboard" replace /> : children;
+};
+
+export default PublicRoute;
